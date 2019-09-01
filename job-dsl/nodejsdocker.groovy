77a85b4798110793d11db8fe09a146322d2b8b69
Mateusz Dz <45406097+42Matt@users.noreply.github.com>
2019-09-01 17:44:37 +0200

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,8 +1,8 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('https://github.com/42Matt/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('42Matt)
+            node / gitConfigEmail('45406097+42Matt@users.noreply.github.com'')
         }
     }
     triggers {
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('42dock/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
